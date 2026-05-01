62acad832a7b441e451cbacbeceafbcf3c20a8c4
PawniSingh <pawnisingh3018@gmail.com>
2026-05-01 14:16:13 +0530

    initial commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -5,4 +5,7 @@ public class hello {
     public static void printSomething(){
         System.out.println("Print something");
     }
+    public static void doSomething(){
+        System.out.println("Do something");
+    }
 }
